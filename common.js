93fbafbaa7823efbdc9eb69e48696f841889e85f
Kimmo Lahtinen <gimblll@gmail.com>
2022-11-24 19:59:40 +0200

    Added Mastodon link to main page too + some random text modifications again

--- common.js
+++ common.js
@@ -31,6 +31,7 @@ function create_header(current_page)
 			  <div class="row justify-content-center">
 		  	 	<div class="col-2"><a href="mailto:gimblll@gmail.com"><i class="bi-envelope gbl-title-icon"></i></a></div>
 		  	 	<div class="col-2"><a href="https://twitter.com/gimblll" target="_blank" rel="noopener noreferrer"><i class="bi-twitter gbl-title-icon"></i></a></div>
+		  	 	<div class="col-2"><a rel="me" href="https://mastodon.gamedev.place/@gimblll" target="_blank" rel="noopener noreferrer"><i class="bi-mastodon gbl-title-icon"></i></a></div>
 		  	 	<div class="col-2"><a href="https://fi.linkedin.com/in/kimmo-lahtinen-908612111" target="_blank" rel="noopener noreferrer"><i class="bi-linkedin gbl-title-icon"></i></a></div>
 		 	 	</div>
    		</div>
